e4906e05a0cbbc66b6ee6a15605a14f00b6fd53c
bfasenfest <aowbackup@gmail.com>
2015-12-06 14:43:42 -0500

    Sound organzation and percussion addition

--- client/js/build/build.js
+++ client/js/build/build.js
@@ -2581,15 +2581,39 @@ var MainScene = exports.MainScene = (function (_SheenScene) {
     this.mouse = new THREE.Vector2();
     this.tweetMeshes = [];
     this.sounds = {};
-    this.useSkybox = true;
-    this.useMeshImages = true;
+    this.useSkybox = false;
+    this.useMeshImages = false;
     this.useSentimentColor = false;
     this.useRandomColor = true;
+    this.usePercussion = true;
+    this.useInstruments = false;
 
     var soundFilenames = ["altglock1", "altglock2", "altglock3", "altglock4", "altglock5", "altglock6", "altglock7", "altglock8", "badmallet1", "badmallet2", "badmallet3", "badmallet4", "badmallet5", "badmallet6", "badmallet7", "badmallet8", "background1", "background1loud", "bell1", "bell2", "bell3", "bell4", "clouds1", "clouds2", "clouds3", "clouds4", "clouds5", "clouds6", "clouds7", "clouds8", "dbass1", "dbass2", "dbass3", "dbass4", "dbass5", "dbass6", "dbass7", "dbass8", "glock1", "glock2", "glock3", "glock4", "glock5", "glock6", "glock7", "glock8", "glock9", "glock10", "glock11", "glock12", "glock13", "mallet1", "mallet2", "mallet3", "mallet4", "mallet5", "mallet6", "mallet7", "mallet8", "tile1", "tile2", "tile3", "tile4", "tile5", "tile6", "tile7", "tile8"];
     soundFilenames.forEach(function (filename) {
-      var sound = new buzz.sound("/media/sound/" + filename, {
+      var sound = new buzz.sound("/media/sound/instruments/" + filename, {
         formats: ["mp3", "ogg"],
+        webAudioApi: true,
+        volume: 30
+      });
+      _this.sounds[filename] = sound;
+    });
+
+    var soundFilenames2 = [];
+    for (var i = 0; i < 31; i++) {
+      var soundnum = i + 1;
+      var sound = "hh" + soundnum;
+      soundFilenames2.push(sound);
+    }
+
+    for (var i = 0; i < 19; i++) {
+      var soundnum = i + 1;
+      var sound = "kick" + soundnum;
+      soundFilenames2.push(sound);
+    }
+
+    soundFilenames2.forEach(function (filename) {
+      var sound = new buzz.sound("/media/sound/percussion/" + filename, {
+        formats: ["mp3"],
         webAudioApi: true,
         volume: 30
       });
@@ -2599,7 +2623,7 @@ var MainScene = exports.MainScene = (function (_SheenScene) {
     this.detailTweetTextElement = document.querySelector("#detail-tweet-text");
     this.sounds.background1loud.setVolume(70);
     this.sounds.background1loud.setTime(0);
-    //this.sounds.background1loud.play();
+    this.sounds.background1loud.play();
 
     this.socket = io("http://localhost:6001");
     this.socket.on("fresh-tweet", this.handleNewTweet.bind(this));
@@ -2814,74 +2838,109 @@ var MainScene = exports.MainScene = (function (_SheenScene) {
     makeGodSound: {
       value: function makeGodSound(score) {
         var sounds = this.sounds;
-        var soundarray = [];
+        var soundArray = [];
+        var percSoundArray = [];
 
         var sound;
+        var sound2;
+
         if (score > 15) {
-          sound = sounds.glock13;
+          soundArray = [sounds.glock13];
+          percSoundArray = [sounds.hh28, sounds.hh29, sounds.hh30, sounds.hh31, sounds.hh32];
         } else if (score > 14) {
-          sound = sounds.glock12;
+          soundArray = [sounds.glock12];
+          percSoundArray = [sounds.hh27, sounds.hh28, sounds.hh29, sounds.hh30, sounds.hh31];
         } else if (score > 13) {
-          sound = sounds.glock11;
+          soundArray = [sounds.glock11];
+          percSoundArray = [sounds.hh26, sounds.hh27, sounds.hh28, sounds.hh29, sounds.hh30];
         } else if (score > 12) {
-          sound = sounds.glock10;
+          soundArray = [sounds.glock10];
+          percSoundArray = [sounds.hh25, sounds.hh26, sounds.hh27, sounds.hh28, sounds.hh29];
         } else if (score > 11) {
-          sound = sounds.glock9;
+          soundArray = [sounds.glock9];
+          percSoundArray = [sounds.hh24, sounds.hh25, sounds.hh26, sounds.hh27, sounds.hh28];
         } else if (score > 10) {
-          sound = sounds.glock8;
+          soundArray = [sounds.glock8];
+          percSoundArray = [sounds.hh23, sounds.hh24, sounds.hh25, sounds.hh26, sounds.hh27];
         } else if (score > 9) {
-          sound = sounds.glock7;
+          soundArray = [sounds.glock7];
+          percSoundArray = [sounds.hh22, sounds.hh23, sounds.hh24, sounds.hh25, sounds.hh26];
         } else if (score > 8) {
-          sound = sounds.altglock6;
+          soundArray = [sounds.altglock6];
+          percSoundArray = [sounds.hh21, sounds.hh22, sounds.hh23, sounds.hh24, sounds.hh25];
         } else if (score > 7) {
-          sound = sounds.altglock5;
+          soundArray = [sounds.altglock5];
+          percSoundArray = [sounds.hh20, sounds.hh21, sounds.hh22, sounds.hh23, sounds.hh24];
         } else if (score > 6) {
-          sound = sounds.altglock4;
+          soundArray = [sounds.altglock4];
+          percSoundArray = [sounds.hh10, sounds.hh11, sounds.hh12, sounds.hh13, sounds.hh14];
         } else if (score > 5) {
-          sound = sounds.altglock3;
+          soundArray = [sounds.altglock3];
+          percSoundArray = [sounds.hh9, sounds.hh10, sounds.hh11, sounds.hh12, sounds.hh13];
         } else if (score > 4) {
-          sound = sounds.altglock2;
+          soundArray = [sounds.altglock2];
+          percSoundArray = [sounds.hh8, sounds.hh9, sounds.hh10, sounds.hh11, sounds.hh12];
         } else if (score > 3) {
-          sound = sounds.altglock1;
+          soundArray = [sounds.altglock1];
+          percSoundArray = [sounds.hh7, sounds.hh8, sounds.hh9, sounds.hh10, sounds.hh11];
         } else if (score > 2) {
-          soundarray = [sounds.mallet4, sounds.mallet5, sounds.mallet6, sounds.mallet7, sounds.mallet8];
-          sound = kt.choice(soundarray);
+          soundArray = [sounds.mallet4, sounds.mallet5, sounds.mallet6, sounds.mallet7, sounds.mallet8];
+          percSoundArray = [sounds.hh6, sounds.hh7, sounds.hh8, sounds.hh9, sounds.hh10];
         } else if (score > 1) {
-          soundarray = [sounds.mallet3, sounds.mallet4, sounds.mallet5, sounds.mallet6, sounds.mallet7];
-          sound = kt.choice(soundarray);
+          soundArray = [sounds.mallet3, sounds.mallet4, sounds.mallet5, sounds.mallet6, sounds.mallet7];
+          percSoundArray = [sounds.hh5, sounds.hh6, sounds.hh7, sounds.hh8, sounds.hh9];
         } else if (score > 0) {
-          soundarray = [sounds.mallet2, sounds.mallet3, sounds.mallet4, sounds.mallet5, sounds.mallet6];
-          sound = kt.choice(soundarray);
+          soundArray = [sounds.mallet2, sounds.mallet3, sounds.mallet4, sounds.mallet5, sounds.mallet6];
+          percSoundArray = [sounds.hh4, sounds.hh5, sounds.hh6, sounds.hh7, sounds.hh8];
         } else if (score > -1) {
-          soundarray = [sounds.mallet1, sounds.mallet2, sounds.mallet3, sounds.mallet4, sounds.mallet5];
-          sound = kt.choice(soundarray);
+          soundArray = [sounds.mallet1, sounds.mallet2, sounds.mallet3, sounds.mallet4, sounds.mallet5];
+          percSoundArray = [sounds.hh3, sounds.hh4, sounds.hh5, sounds.hh6, sounds.hh7];
         } else if (score > -2) {
-          soundarray = [sounds.dbass4, sounds.dbass5, sounds.dbass6, sounds.dbass7, sounds.dbass8];
-          sound = kt.choice(soundarray);
+          soundArray = [sounds.dbass4, sounds.dbass5, sounds.dbass6, sounds.dbass7, sounds.dbass8];
+          percSoundArray = [sounds.hh2, sounds.hh3, sounds.hh4, sounds.hh5, sounds.hh6];
         } else if (score > -3) {
-          soundarray = [sounds.dbass1, sounds.dbass2, sounds.dbass3, sounds.dbass4, sounds.dbass5];
-          sound = kt.choice(soundarray);
+          soundArray = [sounds.dbass1, sounds.dbass2, sounds.dbass3, sounds.dbass4, sounds.dbass5];
+          percSoundArray = [sounds.hh1, sounds.hh2, sounds.hh3, sounds.hh4, sounds.hh5];
         } else if (score > -4) {
-          sound = sounds.cloud8;
+          soundArray = [sounds.cloud8];
+          percSoundArray = [sounds.kick17, sounds.kick18, sounds.kick19, sounds.kick20];
         } else if (score > -5) {
-          sound = sounds.clouds7;
+          soundArray = [sounds.clouds7];
+          percSoundArray = [sounds.kick13, sounds.kick14, sounds.kick15, sounds.kick16];
         } else if (score > -6) {
-          sound = sounds.clouds6;
+          soundArray = [sounds.clouds6];
+          percSoundArray = [sounds.kick9, sounds.kick10, sounds.kick11, sounds.kick12];
         } else if (score > -7) {
-          sound = sounds.clouds5;
+          soundArray = [sounds.clouds5];
+          percSoundArray = [sounds.kick6, sounds.kick7, sounds.kick8];
         } else if (score > -8) {
-          sound = sounds.clouds4;
+          soundArray = [sounds.clouds4];
+          percSoundArray = [sounds.kick4, sounds.kick5];
         } else if (score > -9) {
-          sound = sounds.clouds3;
+          soundArray = [sounds.clouds3];
+          percSoundArray = [sounds.kick3];
         } else if (score > -10) {
-          sound = sounds.clouds2;
+          soundArray = [sounds.clouds2];
+          percSoundArray = [sounds.kick2];
         } else {
-          sound = sounds.clouds1;
+          soundArray = [sounds.clouds1];
+          percSoundArray = [sounds.kick1];
         }
 
-        if (sound.isPaused() || sound.getTime() > 0.2) {
-          sound.setTime(0);
-          //sound.play();
+        if (this.useInstruments) {
+          sound = kt.choice(soundArray);
+          if (sound.isPaused() || sound.getTime() > 0.2) {
+            sound.setTime(0);
+            sound.play();
+          }
+        }
+
+        if (this.usePercussion) {
+          sound2 = kt.choice(percSoundArray);
+          if (sound2.isPaused() || sound2.getTime() > 0.2) {
+            sound2.setTime(0);
+            sound2.play();
+          }
         }
       }
     },
